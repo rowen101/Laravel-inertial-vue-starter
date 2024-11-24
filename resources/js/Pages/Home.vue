c4d8f36cb0d6419d1cf32e408b4faed9b9ce3ceb
Rowen <rgrowengonzales66@gmail.com>
2024-11-24 11:58:47 +0800

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+    <header>
+        <p>
+            Stephanie Gonzles
+        </p>
+    </header>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/',function (){
+//     return Inertia::render('Home');
+// });
+
+Route::inertia('/','Home')->name('home');
